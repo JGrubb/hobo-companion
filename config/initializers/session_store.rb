1a0dbc051ae80eedbef3b80506b1e656ba64faaa
John Grubb <therealjohnnygrubb@gmail.com>
2013-03-22 16:32:14 -0400

    init commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RRE::Application.config.session_store :cookie_store, key: '_RRE_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RRE::Application.config.session_store :active_record_store
