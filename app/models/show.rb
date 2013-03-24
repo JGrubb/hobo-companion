d39166ede44b0cae0a486cb26ab5a21602a088a9
John Grubb <therealjohnnygrubb@gmail.com>
2013-03-24 12:21:02 -0400

    adding dependant: :delete_all to songs and shows

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,8 @@ class Show < ActiveRecord::Base
   attr_accessible :date, :show_notes, :venue_id
 
   belongs_to :venue
-  has_many :song_instances
+  
+  has_many :song_instances, :dependant => :delete_all
   has_many :songs, :through => :song_instances
 
   validates :date, :venue_id, :presence => true
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   attr_accessible :author, :is_song, :title
 
-  has_many :song_instances
-  has_many :songs, :through => :song_instances
+  has_many :song_instances, :dependant => :delete_all
+  has_many :shows, :through => :song_instances
 
 end
