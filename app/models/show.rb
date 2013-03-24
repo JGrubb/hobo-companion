8a64af951f25db63427f5b7c1a2f721496945a6d
John Grubb <therealjohnnygrubb@gmail.com>
2013-03-24 12:21:33 -0400

    Typo

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
 
   belongs_to :venue
   
-  has_many :song_instances, :dependant => :delete_all
+  has_many :song_instances, :dependent => :delete_all
   has_many :songs, :through => :song_instances
 
   validates :date, :venue_id, :presence => true
