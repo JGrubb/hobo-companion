d94705aeab2f9774f710184c3998faf3f1480900
John Grubb <therealjohnnygrubb@gmail.com>
2014-01-20 11:12:24 -0500

    fixing conf token from Devise upgrade

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ RRE::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  config.eager_load = false
 
   # Only use best-standards-support built into browsers
   # config.action_dispatch.best_standards_support = :builtin
