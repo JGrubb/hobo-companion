bc72f7f1c2989a3b05e7cf9b061df752693c5a5e
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-15 07:54:50 -0500

    fixing versions_count migration

--- db/migrate/20140207212833_adding_version_count_to_songs.rb
+++ db/migrate/20140207212833_adding_version_count_to_songs.rb
@@ -2,7 +2,7 @@ class AddingVersionCountToSongs < ActiveRecord::Migration
   def up
     add_column :songs, :versions_count, :integer, default: 0
     ids = Set.new
-    Song.where(is_song: true).each { |s| ids << s.id }
+    Song.each { |s| ids << s.id }
     ids.each do |song_id|
       Song.reset_counters(song_id, :versions)
     end
