7990eafc037a5bc170a5b7dd73bde84bd830d24c
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-21 06:49:40 -0500

    merging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     user_name = current_user.first_name.blank? ? "User ##{current_user.id}" : current_user.first_name
   end
 
+  def user_id
+    user_signed_in? ? current_user.id : 0
+  end
+
   def key_maker(str)
     Digest::MD5.hexdigest(str)
   end
